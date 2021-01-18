245007885c101428fb007158a6bf7d8833b1c0aa
deniceLiu <deniceb646@gmail.com>
2021-01-18 17:55:00 +0800

    setup StakingDeploy

---
+++ test/TestStaking.js
@@ -0,0 +1,13 @@
+const AssertionUtils = require("./utils/AssertionUtils.js")
+const AccountUtils = require("./utils/AccountUtils.js")
+const BNUtils = require("./utils/BNUtils.js");
+const timeMachine = require("./utils/TimeMachine.js")
+const deploy = require("./lib/Deploy.js");
+const tokenAddress = require('./const/Token.js');
+
+jest.setTimeout(30 * 60 * 1000);
+
+describe('Test BellaStaking, take USDT for example', () => {
+
+
+})
